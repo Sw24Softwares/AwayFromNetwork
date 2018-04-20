047e004644ff2153f8d98e45678358a9a14ac587
Hamza Parnica <hparnica@gmail.com>
2018-04-20 12:53:48 +0200

    Complete previous commit

---
+++ app/src/main/java/org/sw24softwares/awayfromnetwork/wifi/WifiMainActivity.kt
@@ -0,0 +1,11 @@
+package org.sw24softwares.awayfromnetwork
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WifiMainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_wifi_main)
+        }
+}
